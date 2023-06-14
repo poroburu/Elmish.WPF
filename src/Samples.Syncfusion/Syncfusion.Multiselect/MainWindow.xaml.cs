a382dd1984b9e5922747712de0883e45185c5383
Poroburu <poroburu@gmail.com>
2023-06-14 18:38:51 -0400

    init Samples.Syncfusion
    Improve samples #419
    
    [Syncfusion Community Licence](https://www.syncfusion.com/sales/communitylicense)
    
    * copy directory `Samples` to `Samples.Syncfusion`
    
    * append project directories with `Syncfusion.`

---
+++ src/Samples.Syncfusion/Syncfusion.Multiselect/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Elmish.WPF.Samples.Multiselect {
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+  }
+}
---
+++ src/Samples.Syncfusion/Syncfusion.SubModelSeq/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Elmish.WPF.Samples.SubModelSeq
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
